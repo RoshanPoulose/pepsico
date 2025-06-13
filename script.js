14bb2d1c52a6e03feef1c4e346fa8020c08129c0
RoshanPoulose <roshanpoulose10@gmail.com>
2025-06-13 09:15:38 +0530

    6/13/25

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ bar.addEventListener("click",(e)=>{
     bar.classList.toggle("active")
     clicks.classList.toggle("show")
 })                                  
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".scroll");
+  const items = container.querySelectorAll(".scrolls");
+  let currentIndex = 0;
+
+  function scrollToNext() {
+    currentIndex = (currentIndex + 1) % items.length;
+    const itemWidth = items[0].offsetWidth;
+    container.scrollTo({  
+      left: currentIndex * itemWidth,  
+      behavior: "smooth"
+    });
+  }
+  
+  setInterval(scrollToNext, 10000);
+});
+   
+       
